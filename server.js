9548cf0cd95a1bb1b13049e30a52c53ba197855b
apdanan <apdanan@gmail.com>
2022-10-16 20:46:32 +0800

    updated server port to heroku compatible port

--- server.js
+++ server.js
@@ -80,4 +80,4 @@ app.post('/setrider',urlencodedParser, (req, res) => {
 
 
 
-app.listen(3000, () => console.log('Server running on port 3000!'))
+app.listen(process.env.PORT || 5000, () => console.log('Server running on port 3000!'))
